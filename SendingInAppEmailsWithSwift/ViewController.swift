a364d60d8e75ace14c6d2b1bc3c922a980d9b1bc
Gregory Omoma <gregoryomoma@gregorys-macbook-pro.local>
2019-02-14 16:33:44 -0500

    Initial Commit

---
+++ SendingInAppEmailsWithSwift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SendingInAppEmailsWithSwift
+//
+//  Created by Gregory Omoma on 2/14/19.
+//  Copyright © 2019 Gregory Omoma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
